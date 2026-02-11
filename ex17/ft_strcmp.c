7be9d798130c85cd680d037383ae12299d4960a0
S3RA <63154564+0xS3RA@users.noreply.github.com>
2026-02-11 13:48:27 +0100

    Update ft_strcmp.c

--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -1,16 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vvan-ach <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/10 14:52:25 by vvan-ach          #+#    #+#             */
-/*   Updated: 2025/12/10 16:18:16 by vvan-ach         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-//#include <stdio.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -21,11 +8,3 @@ int	ft_strcmp(char *s1, char *s2)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-
-//int	main(void)
-//{
-//	char str[] = "bonjour";
-//	char str2[] = "bonjour";
-//	printf("%i\n", ft_strcmp(str, str2));
-//	return (0);
-//}
